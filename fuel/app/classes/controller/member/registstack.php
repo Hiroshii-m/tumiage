a68ea46bc6b288f8ce34044566c8278deb43d38c
ひろし <hirohiroshi823@gmail.com>
2021-05-29 22:01:05 +0900

    記録登録ページで月、年合計と言った情報をTwitterへ上げれるようにしました。マイページのHTML、CSSを作成しました。
    ただ、今のままだと、360レコードものデータを取得して編集といったDBにとても負荷のかかる作りなので、DB設計と処理フローの書き出しから見直しをしました。

--- fuel/app/classes/controller/member/registstack.php
+++ fuel/app/classes/controller/member/registstack.php
@@ -30,6 +30,9 @@ class Controller_Member_Registstack extends Controller_Member
                 $data['user_id'] = Arr::get(Auth::get_user_id(), 1);
                 $data['created_at'] = $formData['created_at'];
                 $data['text_num'] = $formData['text_num'];
+                // 入力したテキストの改行コードをTwitter用に置き換える
+                $content = str_replace(array("\r\n", "\r", "\n"), '%0a', $formData['content']);
+
                 // 登録した年初末日を取得
                 $new_day = date('Y', strtotime($data['created_at'])).'-01-01';
                 $last_day = date('Y', strtotime($data['created_at'])).'-12-31';
@@ -38,23 +41,35 @@ class Controller_Member_Registstack extends Controller_Member
                     'select' => array('id', 'user_id', 'text_num', 'created_at'),
                     'where' => array(
                         'user_id' => $data['user_id'],
-                        array('created_at', 'between', array($new_day,$last_day))
+                        array('created_at', 'between', array($new_day,$last_day)),
+                        'delete_flg' => 0
                     ),
                 ));
                 // 今日と1月分のデータを取得
                 $this_month = date('Y-m', strtotime($data['created_at']));
                 $month_sum = 0;
+                $year_sum = 0;
                 foreach($post as $key => $val){
-                    // 今日のデータの場合
+                    // 今日のデータを格納
                     if($post[$key]->created_at === $data['created_at']){
                         $post_today = $post[$key];
                     }
-                    // 今月のデータの場合
+                    // 今月の合計文字数を計算
                     if(date('Y-m', strtotime($post[$key]->created_at)) === $this_month){
                         $month_data[] = $post[$key];
-                        $month_sum =+ $post[$key]->text_num;
+                        $month_sum += $post[$key]->text_num;
                     }
+                    // 今年の合計文字数を計算
+                    $year_sum += $post[$key]->text_num;
                 }
+                // 今月のデータのレコード数を格納
+                $month_count = count($month_data);
+                // 今月の平均文字数を計算
+                $month_average = round($month_sum/$month_count, 2);
+                // 今年のレコード数を格納
+                $year_count = count($post);
+                // 今年の平均文字数を計算
+                $year_average = round($year_sum/$year_count, 2);
                 
                 if(!empty($post_today)){
                     // データ更新の準備　postからcreated_atの日付だけ変更
@@ -62,12 +77,12 @@ class Controller_Member_Registstack extends Controller_Member
                     $post_today->text_num = $data['text_num'];
                 }else{
                     // データ挿入準備
-                    $post = \Model\Stack::forge(); // Model_Stackオブジェクトを生成
-                    $post->set($data); // インスタンスに値の配列をセット
+                    $post_today = \Model\Stack::forge(); // Model_Stackオブジェクトを生成
+                    $post_today->set($data); // インスタンスに値の配列をセット
                 }
                 $post_today->save(); // レコードを挿入または更新をします
                 Session::set_flash('sucMsg', 'データ登録/更新に成功しました。');
-                $url = 'https://twitter.com/intent/tweet?text='.date('m月d日', strtotime($data['created_at'])).'%0a'.'文字数：'.$data['text_num'];
+                $url = 'https://twitter.com/intent/tweet?text='.date('m月d日', strtotime($data['created_at'])).'%0a本日：'.$data['text_num'].'文字%0a月平均：'.$month_average.'文字%0a月合計：'.$month_sum.'文字%0a年平均：'.$year_average.'文字%0a年合計：'.$year_sum.'文字%0a%0a'.$content;
                 // Twitterへ投稿
                 Response::redirect($url);
             // バリデーション失敗
--- fuel/app/views/template/head.php
+++ fuel/app/views/template/head.php
@@ -2,6 +2,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?= Asset::css('style.css'); ?>
+    <?= Asset::css('fontawesome/css/all.css') ?>
+    <?= Asset::css('style.css') ?>
     <title>記録アップ!</title>
 </head>
